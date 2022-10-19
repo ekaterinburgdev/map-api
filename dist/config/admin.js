ed091083d552c0f48b2f12b472844796ad319cf7
Kseniia Astakhova <astakhova.ksyusha.07@mail.ru>
2022-10-19 23:29:20 +0500

    docker-compose fixes

--- dist/config/admin.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ({ env }) => ({
-    auth: {
-        secret: env('ADMIN_JWT_SECRET', '320fa763704025a2ab3f4f125cd056d8'),
-    },
-});
