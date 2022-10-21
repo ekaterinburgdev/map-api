a66567f2df8cd5f3a317ce9856e94dba49d520ee
Kseniia Astakhova <astakhova.ksyusha.07@mail.ru>
2022-10-22 00:44:44 +0500

    made volume for src, enabled plugin for import/export data

---
+++ config/plugin.ts
@@ -0,0 +1,5 @@
+export default ({ env }) => ({
+    'import-export-entries': {
+      enabled: true,
+    }
+  });
--- src/admin/webpack.config.ts
+++ src/admin/webpack.config.ts
@@ -1,6 +1,12 @@
+'use strict';
+
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
 export default (config, webpack) => {
   // Note: we provide webpack above so you should not `require` it
   // Perform customizations to webpack config
   // Important: return the modified config
+  config.plugins.push(new MonacoWebpackPlugin());
+
   return config;
 };
