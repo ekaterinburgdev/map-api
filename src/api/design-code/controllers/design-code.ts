6da0f13c9c2b11c523644789f4d5828916305466
root <root@17520.ovh.cccp.cloud>
2023-01-22 23:00:02 +0300

    Automatically synchronized with admin interface

---
+++ src/api/design-code/controllers/design-code.ts
@@ -0,0 +1,7 @@
+/**
+ *  design-code controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::design-code.design-code');
---
+++ src/api/design-code/routes/design-code.ts
@@ -0,0 +1,7 @@
+/**
+ * design-code router.
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::design-code.design-code');
